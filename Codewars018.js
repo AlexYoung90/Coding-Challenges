a396810b97d1ca4744b78336da66f30d9fe17ebb
AlexYoung90 <ajyoung1990@gmail.com>
2022-07-01 22:30:30 -0400

    Codewars Coding Challenge

---
+++ Codewars018.js
@@ -0,0 +1,20 @@
+/* a hard quarter in the office you decide to get some rest on a vacation. So you will book a flight for you and your girlfriend and try to leave all the mess behind you.
+
+You will need a rental car in order for you to get around in your vacation. The manager of the car rental makes you some good offers.
+
+Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total. Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
+
+Write a code that gives out the total amount for different days(d).*/
+
+
+function rentalCarCost(d) {// function to check our daily total
+    if( d >= 7 ){// if the days rented equal 7 or more
+      return (d * 40) - 50// day times daily rate minus 50 for the 7 day bonus 
+    }
+    else if (d >= 3){// same as above checking for 3 day rate
+      return (d * 40) - 20
+    }
+    else{ // if less then 3 chargin the regular rate.
+      return d * 40
+    }
+  }
