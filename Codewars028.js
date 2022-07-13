59356d378f5b09fab91b65964c37b7c17d55d1fa
AlexYoung90 <ajyoung1990@gmail.com>
2022-07-13 15:11:06 -0400

    Codewars Coding Challenge

---
+++ Codewars028.js
@@ -0,0 +1,11 @@
+/**Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :) */
+
+// an easy way to make this function always return 5 would be to create an array with 5 items in it and then return the length 
+function unusualFive() {
+    const arrayOfFive = ['item','item','item','item','item']
+    return arrayOfFive.length
+  }
