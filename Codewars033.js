f2ac966a901538ad3e9acff75d93d59a6edc94c1
AlexYoung90 <ajyoung1990@gmail.com>
2022-07-26 17:51:23 -0400

    Codewars Coding Challange

---
+++ Codewars033.js
@@ -0,0 +1,8 @@
+/**Implement a function which convert the given boolean value into its string representation.
+for true return 'true'
+for false return 'false'
+Note: Only valid inputs will be given. */
+
+function booleanToString(b){
+    return b === true ? 'true': 'false'
+  }
